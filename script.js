34975e291854a606510d1f84e7c8fa4e3823a807
Dragon_Fly <143097451+gurpreetsingh911@users.noreply.github.com>
2025-05-31 15:45:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,21 +7,33 @@ let second = document.getElementById("name");
 sub.addEventListener('click', function(event) {
 event.preventDefault();
   let firVal = first.value;
-  let secVal = second.value;
+  let secVal = second.value.trim();
   if(firVal == 0 || secVal.length == 0 ){
   	alert("Please enter valid details.")  
      return;
   }
   
 
-  let promise = new Promise((resole, reject)=>{
-  if(firVal >= 18){ 
-  setTimeout(()=> alert(`Welcome, ${secVal}. You can vote.`), 4000)  
-  }else{
-    setTimeout(()=> alert(`Oh sorry ${secVal}. You aren't old enough.`), 4000)  
-  }
-
-}); 
+  let promise = new Promise((resolve, reject) => {
+    if (age >= 18) {
+      setTimeout(() => resolve(`Welcome, ${name}. You can vote.`), 4000);
+    } else {
+      setTimeout(() => reject(`Oh sorry ${name}. You aren't old enough.`), 4000);
+    }
+  })
+  .then(message => alert(message))
+  .catch(message => alert(message));
 	
 });
+
+
+
+
+
+
+
+
+
+
+
 
